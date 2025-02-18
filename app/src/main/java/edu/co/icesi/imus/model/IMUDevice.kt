24ddd0de755a7592e28e96f7772a611af8f3b45d
Santiago0212004 <sjbs0212@gmail.com>
2025-02-18 09:39:44 -0500

    Added IMUDevice enum class to store the allowed names of IMUs for bluetooth implementation

---
+++ app/src/main/java/edu/co/icesi/imus/model/IMUDevice.kt
@@ -0,0 +1,9 @@
+package edu.co.icesi.imus.model
+
+enum class IMUDevice(val bluetoothName: String) {
+    LEFT_HAND("LEFT-HAND"),
+    RIGHT_HAND("RIGHT-HAND"),
+    LEFT_ANKLE("LEFT-ANKLE"),
+    RIGHT_ANKLE("RIGHT-ANKLE"),
+    BASE_SPINE("BASE-SPINE")
+}
